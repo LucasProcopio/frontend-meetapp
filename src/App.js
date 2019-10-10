2d0f7bc735870b97b2a879152bc1348cb99539fd
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-10 10:39:38 -0300

    :fire: feat: Add redux persisto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import './config/ReactotronConfig';
 
-import { store } from './store';
+import { store, persistor } from './store';
 import Routes from './routes';
 import history from './services/history';
 import GlobalStyle from './styles/global';
@@ -12,10 +13,12 @@ import GlobalStyle from './styles/global';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
       <ToastContainer position="top-center" autoClose={2800} />
     </Provider>
   );
